5795818355f83361b87f368bc882e064f54bf709
ArtemCH240322 <sun2383@mail.ru>
2022-05-30 12:40:16 +0300

    Первый комит. Задание 5.

--- src/ru/skypro/Main.java
+++ src/ru/skypro/Main.java
@@ -3,6 +3,11 @@ package ru.skypro;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        int value = 33;
+        changeVolue(value);
+        System.out.println("Задание 5 = " + value);
+    }
+    private static void changeVolue(int value) {
+
     }
 }
